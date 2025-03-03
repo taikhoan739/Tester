9616a049e2ed2cf3af2386f51829b1b44184d5fc
unknown <prolathe633@gmail.com>
2025-03-03 13:03:50 +0700

    init project

---
+++ TesterDemo/src/main/java/daihocmo/testerdemo/TesterDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daihocmo.testerdemo;
+
+/**
+ *
+ * @author admin
+ */
+public class TesterDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
